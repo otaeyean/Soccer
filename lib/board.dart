f4fdb9aeb6e19c8bc9ad6eb6c9df3d32da123357
otaeyean <oty12125@naver.com>
2024-12-03 20:55:51 +0900

    update: main에서 네비게이션바 수정, 게시판 ui 수정

--- lib/board.dart
+++ lib/board.dart
@@ -12,21 +12,20 @@ class BoardPage extends StatefulWidget {
 
 class _BoardPageState extends State<BoardPage> {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  bool _isLoggedIn = false;  // 로그인 여부를 관리할 변수ㅅ
+  bool _isLoggedIn = false;
 
   @override
   void initState() {
     super.initState();
-    _checkLoginStatus();  // 로그인 상태 확인
+    _checkLoginStatus();
   }
 
-  // SharedPreferences에서 로그인 상태 확인
   Future<void> _checkLoginStatus() async {
     final prefs = await SharedPreferences.getInstance();
     final userName = prefs.getString('userName');
 
     setState(() {
-      _isLoggedIn = userName != null && userName.isNotEmpty;  // userName 값이 있으면 로그인 상태
+      _isLoggedIn = userName != null && userName.isNotEmpty;
     });
   }
 
@@ -35,9 +34,17 @@ class _BoardPageState extends State<BoardPage> {
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text('자유게시판', style: TextStyle(fontSize: 20, fontFamily: "GmarketBold")),
+          child: Text(
+            '자유게시판',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: "GmarketBold",
+              color: Colors.white,  // 텍스트 색상을 흰색으로 변경
+            ),
+          ),
         ),
         automaticallyImplyLeading: false,
+        backgroundColor:const Color.fromARGB(255, 20, 40, 153),  // 네이비 색상 배경 적용
       ),
       body: StreamBuilder<QuerySnapshot>(
         stream: _firestore.collection('posts').orderBy('timestamp', descending: true).snapshots(),
@@ -53,10 +60,21 @@ class _BoardPageState extends State<BoardPage> {
                 child: Card(
                   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
                   child: ListTile(
-                    title: Text(post['title'], style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: "GmarketBold")),
+                    title: Text(
+                      post['title'],
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "GmarketBold",
+                      ),
+                    ),
                     subtitle: Text(
                       '${post['username']} • 댓글 ${post['commentCount']}개 • ${post['timestamp'].toDate().toString().substring(0, 16)}',
-                      style: TextStyle(fontSize: 14, color: Colors.grey, fontFamily: "GmarketMedium"),
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.grey,
+                        fontFamily: "GmarketMedium",
+                      ),
                     ),
                     trailing: Icon(Icons.arrow_forward_ios, size: 16, color: Colors.grey),
                     onTap: () {
@@ -75,11 +93,11 @@ class _BoardPageState extends State<BoardPage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _isLoggedIn
             ? () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => CreatePostPage()));
-        }
-            : null,  // 로그인되지 않은 경우 버튼을 비활성화
-        child: Icon(Icons.add),
-        backgroundColor: _isLoggedIn ? Colors.blue : Colors.grey,  // 로그인 상태에 따라 버튼 색상 변경
+                Navigator.push(context, MaterialPageRoute(builder: (context) => CreatePostPage()));
+              }
+            : null,
+        child: Icon(Icons.add, color: Colors.white),  // 아이콘 색상을 흰색으로 변경
+        backgroundColor: _isLoggedIn ? const Color.fromARGB(255, 20, 40, 153) : Colors.grey,  // 네이비 색상 또는 회색 적용
       ),
     );
   }
